fccac1a6491dd6dacc4d00aa070d001a9dceddad
Viktor Radulov <golova@golova.dev>
2022-01-31 14:46:51 +0200

    Removed referer header

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ cors_proxy.createServer({
     'via',
     'connect-time',
     'total-route-time',
+    'referer'
     // Other Heroku added debug headers
     // 'x-forwarded-for',
     // 'x-forwarded-proto',
